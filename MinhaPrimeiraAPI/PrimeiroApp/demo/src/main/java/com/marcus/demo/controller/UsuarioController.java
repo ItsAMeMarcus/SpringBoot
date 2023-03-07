013a844664623de9579dbe5d907cc62d90a8b95a
ItsAMeMarcus <mrmavicico@gmail.com>
2023-03-07 12:45:08 -0300

    MinhaPrimeiraAPI completa

--- MinhaPrimeiraAPI/PrimeiroApp/demo/src/main/java/com/marcus/demo/controller/UsuarioController.java
+++ MinhaPrimeiraAPI/PrimeiroApp/demo/src/main/java/com/marcus/demo/controller/UsuarioController.java
@@ -1,11 +1,19 @@
 package com.marcus.demo.controller;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcus.demo.domain.Usuario;
 import com.marcus.demo.services.UsuarioService;
@@ -19,7 +27,35 @@ public class UsuarioController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Usuario> findById(@PathVariable Integer id){
 		Usuario obj = this.service.findById(id);
-		
-		return null;
+		return ResponseEntity.ok().body(obj);
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> findAll(){
+		List<Usuario> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> update(@PathVariable Integer id,@RequestBody Usuario obj){
+		Usuario newObj = this.service.update(id, obj);
+		
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> create (@RequestBody Usuario obj){
+		Usuario newObj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete (@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 }
--- MinhaPrimeiraAPI/PrimeiroApp/demo/src/main/java/com/marcus/demo/services/UsuarioService.java
+++ MinhaPrimeiraAPI/PrimeiroApp/demo/src/main/java/com/marcus/demo/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.marcus.demo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.marcus.demo.domain.Usuario;
 import com.marcus.demo.repository.UsuarioRepository;
+import com.marcus.demo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UsuarioService {
@@ -17,6 +19,33 @@ public class UsuarioService {
 	public Usuario findById(Integer id) {
 		Optional<Usuario> obj = repository.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Usuario.class.getName()));
+	}
+
+	public List<Usuario> findAll() {
+		return repository.findAll();
+	}
+
+	public Usuario update(Integer id, Usuario obj) {
+		Usuario newObj = findById(id);
+		
+		newObj.setNome(obj.getNome());
+		newObj.setLogin(obj.getLogin());
+		newObj.setSenha(obj.getSenha());
+		repository.save(newObj);
+		
+		return newObj;
+	}
+	
+	public Usuario create (Usuario obj) {
+		obj.setId(null);
+		
+		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		findById(id); 
+		repository.deleteById(id);
 	}
 }

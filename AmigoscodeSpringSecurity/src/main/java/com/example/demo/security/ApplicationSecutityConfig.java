af229b18a1d96076aeefc745ef58b7eb1d409a3e
ItsAMeMarcus <mrmavicico@gmail.com>
2023-08-10 13:45:49 -0300

    ant matchers - request matchers

---
+++ AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecutityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((authz) ->
+                        authz
+                        .requestMatchers("index.html","/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}

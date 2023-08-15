fce360eef1ee1dfc4d51edf21d436fb01e4d2342
ItsAMeMarcus <mrmavicico@gmail.com>
2023-08-15 19:19:52 -0300

    passwordEncoder lesson

--- AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
+++ AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
@@ -1,14 +1,14 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,6 +17,14 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecutityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecutityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -34,7 +42,7 @@ public class ApplicationSecutityConfig {
 
         UserDetails someoneUser = User.builder()
                 .username("Someone")
-                .password("password")
+                .password(passwordEncoder.encode("password"))
                 .roles("STUDENT")
                 .build();
 

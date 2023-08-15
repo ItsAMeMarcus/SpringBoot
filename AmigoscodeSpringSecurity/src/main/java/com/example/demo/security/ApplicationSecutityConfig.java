dd135246f77c5332ef737910f1109329c4dd91e6
ItsAMeMarcus <mrmavicico@gmail.com>
2023-08-15 19:03:30 -0300

    userDetailService lesson

--- AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
+++ AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -24,4 +28,19 @@ public class ApplicationSecutityConfig {
                 .httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    UserDetailsService customUserDetailsService() {
+
+        UserDetails someoneUser = User.builder()
+                .username("Someone")
+                .password("password")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                someoneUser
+        );
+    }
+
 }

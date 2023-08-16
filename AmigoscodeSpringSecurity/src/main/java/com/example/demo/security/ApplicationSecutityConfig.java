2a230f8b18abc586f734c8bf44462278c1342031
ItsAMeMarcus <mrmavicico@gmail.com>
2023-08-16 12:45:16 -0300

    hasAutority_lesson

--- AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
+++ AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.demo.security.ApplicationUserPermission.*;
 import static com.example.demo.security.ApplicationUserRole.*;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -34,6 +36,10 @@ public class ApplicationSecutityConfig {
                         authz
                         .requestMatchers("index.html","/").permitAll()
                         .requestMatchers("/api/**").hasRole(STUDENT.name())
+                        .requestMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                        .requestMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                        .requestMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                        .requestMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());

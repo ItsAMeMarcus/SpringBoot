46b5c2f2386062fe971b5d868239639a64404584
ItsAMeMarcus <mrmavicico@gmail.com>
2023-08-25 22:25:32 -0300

    form_Based_Auth_lesson

--- AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
+++ AmigoscodeSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecutityConfig.java
@@ -42,7 +42,7 @@ public class ApplicationSecutityConfig {
                         .requestMatchers("/api/**").hasRole(STUDENT.name())
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());
+                .formLogin(withDefaults());
         return http.build();
     }
 
